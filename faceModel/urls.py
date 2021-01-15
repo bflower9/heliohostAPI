625c312c4d5bc097f516ae93a722f308758b5b08
Mahmudul Alam <mahmudula2000@gmail.com>
2021-01-15 16:41:26 +0600

    Remove JsonField, back Q

--- faceModel/urls.py
+++ faceModel/urls.py
@@ -1,6 +1,11 @@
-# router = DefaultRouter()
-# router.register(r'', views.FaceModelsViewSet, 'attendance')
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from faceModel import views
+
+router = DefaultRouter()
+router.register(r'', views.FaceModelsViewSet, 'attendance')
 
 urlpatterns = [
-    # path('', include(router.urls)),
+    path('', include(router.urls)),
 ]
