e18cbe6bdd1cba3a6240a894b3921dfe1cda8872
Mahmudul Alam <mahmudula2000@gmail.com>
2021-01-15 16:27:16 +0600

    removed corsheader and rest framework

--- heliohostAPI/urls.py
+++ heliohostAPI/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('', lambda x: HttpResponse('Welcome to api.mahbd.heliohost.us')),
     path('admin/', admin.site.urls),
-    path('face_models/', include('faceModel.urls')),
+    # path('face_models/', include('faceModel.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
