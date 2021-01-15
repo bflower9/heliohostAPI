65bfde47db554c55ef15d5a85a50887a2564452e
Mahmudul Alam <mahmudula2000@gmail.com>
2021-01-15 18:19:17 +0600

    Confirmed JSONField backport

--- faceModel/admin.py
+++ faceModel/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+
+from faceModel.models import FaceModels
+
+
+class FaceAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name', 'user__username', 'user__first_name', 'user__last_name')
+
+
+admin.site.register(FaceModels, FaceAdmin)
