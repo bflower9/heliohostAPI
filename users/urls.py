755ba1581ed9d84e6c9e8bb34075e5328fc37a9e
Mahmudul Alam <mahmudula2000@gmail.com>
2021-01-17 12:19:21 +0600

    Removed is logged in function

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,3 @@
-from django.http import JsonResponse
 from django.urls import path, include
 
 from users import views
@@ -7,5 +6,4 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', views.Register.as_view(), name='register'),
     path('get-token/', views.get_token),
-    path('is_logged/', lambda x: JsonResponse({'result': x.user.is_authenticated}))
 ]
